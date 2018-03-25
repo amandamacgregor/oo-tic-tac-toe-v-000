c6bfc4c2422df0f6a2150174a623b3138c2c99b9
Amanda M <amandaseagull3@gmail.com>
2018-03-25 19:29:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ def position_taken?(index)
   @board[index]== "X" || @board[index] == "O"
 end
 
-def valid_move?(board, index)
+def valid_move?(index)
   index.between?(0,8) && !position_taken?(board, index)
 end
  
