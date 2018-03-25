5ffc71e5a02a36ec9353e8da82963093fe81c718
Amanda M <amandaseagull3@gmail.com>
2018-03-25 19:19:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,6 +69,10 @@ end
 def draw?(board)
   !won?(board) && full?(board)
 end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
