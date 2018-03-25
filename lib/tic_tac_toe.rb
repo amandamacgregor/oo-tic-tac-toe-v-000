5aa45499e8fb0cfc42f9b0ba4602ae08da024e25
Amanda M <amandaseagull3@gmail.com>
2018-03-25 19:17:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,10 @@ class TicTacToe
  def input_to_index(user_input)
   user_input.to_i - 1
 end
+
+def move(board, index, player)
+  board[index] = player
+end
  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
